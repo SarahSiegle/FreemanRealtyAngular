4aa4164ea1298bb1da60ffcaa7d86f7b232ddce7
Sarah Siegle <sarahsiegle@gmail.com>
2015-11-18 09:20:12 -0500

    Trying database connection

---
+++ api/v1/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'cifa0500');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'angularcode_products');
+
+?>
